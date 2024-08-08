c92db12dbc953210a4f38a26e7bef02cd1d524fc
rhansen3 <rjhansen@umich.edu>
2024-08-07 22:22:01 -0400

    Bullet despawning

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    private float timeAlive = 0f;
+    private float maxSeconds = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+        if(timeAlive >= maxSeconds){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
